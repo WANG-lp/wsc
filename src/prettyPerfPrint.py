452244f4e9e2e3d909afb263b22514b615aebea4
Naoya Inoue@Whiskey <naoya-i@ecei.tohoku.ac.jp>
2014-05-07 18:50:15 +0900

    prettyPrint now support PMI+con scheme.

--- src/prettyPerfPrint.py
+++ src/prettyPerfPrint.py
@@ -7,7 +7,7 @@ f = open("graph.tmp", "w")
 for ln in sys.stdin:
 	ln = ln.split("\t")
 	
-	print >>f, "\t".join([ln[0], ln[1+12*0+7], ln[1+12*1+7], ln[1+12*2+7], ln[1+12*3+7]])
+	print >>f, "\t".join([ln[0], ln[1+12*0+7], ln[1+12*1+7], ln[1+12*2+7], ln[1+12*3+7], ln[1+12*4+7]])
 
 f.close()
 
@@ -21,7 +21,8 @@ set ylabel "Acc.";
 plot "graph.tmp" using 1:2 with lp title "Baseline", \
      "graph.tmp" using 1:3 with lp title "PMI", \
      "graph.tmp" using 1:4 with lp title "PMI+arg", \
-     "graph.tmp" using 1:5 with lp title "PMI+arg+con"
+     "graph.tmp" using 1:5 with lp title "PMI+arg+con", \
+     "graph.tmp" using 1:6 with lp title "PMI+con"
 """
 
 f.close()
