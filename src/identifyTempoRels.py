b8cda2c74da902211efdc9151a13f327b0950ded
Naoya Inoue@Whiskey <naoya-i@ecei.tohoku.ac.jp>
2015-08-06 15:05:47 +0900

    Added temporal relation estimation module.

---
+++ src/identifyTempoRels.py
@@ -0,0 +1,27 @@
+#
+# Usage: python identifyTempoRels.py data/dp-train.tuples
+
+import sys
+import re
+
+# Define heuristic rules.
+list_prn_ant = "because|since| if |when| although | though | as "
+list_ant_prn = "When| so | but | and "
+
+
+def _estimateTempoRel(x):
+	if re.search(list_prn_ant, x): return "PRN -> ANT"
+	if re.search(list_ant_prn, x): return "ANT -> PRN"
+
+	return "?"
+
+
+def main():
+	for ln in sys.stdin:
+		t = eval(ln)
+
+		print "\t".join([t[1], _estimateTempoRel(t[1])])
+
+
+if "__main__" == __name__:
+	main()
