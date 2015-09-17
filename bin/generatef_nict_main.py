761d4a82a071b88836c4af7e2944db2df7349cad
Jun SUGIURA <jun-s@ecei.tohoku.ac.jp>
2015-09-17 17:22:47 +0900

    debug for nict

--- bin/generatef_nict_main.py
+++ bin/generatef_nict_main.py
@@ -3,17 +3,18 @@ import subprocess
 import optparse
 import os
 
+def getpnum(setname):
+    cmd = 'wc -l data/dp-%s.tuples' %setname
+    print cmd
+    ret = subprocess.check_output(cmd, shell=True)
+    return int(ret.split(" ")[0])
+
 cmdparser		= optparse.OptionParser(description="Feature generator.")
 cmdparser.add_option("--mpiopt", help = "options for mpirun")
 cmdparser.add_option("--targetset", help = "target set")
 cmdparser.add_option("--genoptions", help = "options for feature generate")
 cmdparser.add_option("--date", help = ".", default="0000")
 options, args = cmdparser.parse_args()
-
-def getpnum(setname):
-    cmd = 'wc -l data/dp-%s.tuples' %setname
-    ret = subprocess.check_output(cmd, shell=True)
-    return int(ret.split(" ")[0])
 
 for setname in options.targetset.split():
     pnum = getpnum(setname) -1
