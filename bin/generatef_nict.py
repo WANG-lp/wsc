a12f6052812a0343cc93170d0c0df0773c2399d6
Jun SUGIURA <jun-s@ecei.tohoku.ac.jp>
2015-09-17 17:11:16 +0900

    add for nict

--- bin/generatef_nict.py
+++ bin/generatef_nict.py
@@ -1,15 +1,12 @@
 import sys
 import subprocess
 import optparse
+import os
 
 def getpnum(setname):
     cmd = 'wc -l data/dp-%s.tuples' %setname
     ret = subprocess.check_output(cmd, shell=True)
     return int(ret.split(" ")[0])
-
-# def getkbname(options):
-#     ret = set("kb4e2 kb0909 kbsmall kb4 kb4e kb4e2down kb87ei kb100 kb10 oldkb".split()) & set(options.split())
-#     return list(ret)[0]
 
 def getprocerr(errfilename):
     cmd = 'fgrep "Processing No" %s |wc -l' % errfilename
@@ -25,30 +22,33 @@ cmdparser.add_option("--date", help = ".", default="0000")
 
 options, args = cmdparser.parse_args()
 # settings
-defaultgenoptions = "--quicktest --extkb /work/jun-s/kb"
+defaultgenoptions = "--quicktest --extkb /share01/jun-s/kb"
 myoptions = "--" + " --".join(options.genoptions.split())
 lightfnlst = []
+
+problemindex = os.environ['BKP_INDEX']
+
 
 for setname in options.targetset.split():
     pnum = getpnum(setname) -1
     print setname, pnum
 
-    xmlfn = "/work/jun-s/tmp/%s.%s.%s.xml" % (options.date, "-".join(options.genoptions.split()), setname)
-    errfn = "/work/jun-s/tmp/%serr" % setname
+    xmlfn = "/share01/jun-s/tmp/%s.%s.%s%s.xml" % (options.date, "-".join(options.genoptions.split()), setname, problemindex)
+    errfn = "/share01/jun-s/tmp/%s%serr" % (setname, problemindex)
 
-    generatecmd = "seq 0 %d | /home/jun-s/bin/parallel -j12 'python src/generateTrainingSet.py --input data/dp-%s.tuples %s --problemno {} %s' > %s 2> %s" %(pnum, setname, defaultgenoptions, myoptions, xmlfn, errfn)
+    generatecmd = "python src/generateTrainingSet.py --input data/dp-%s.tuples %s --problemno %s %s > %s 2> %s" %(setname, defaultgenoptions, problemindex, myoptions, xmlfn, errfn)
     print generatecmd
-    subprocess.call(generatecmd, shell=True)
+    # subprocess.call(generatecmd, shell=True)
 
-    procnum, errnum = getprocerr(errfn)
+    # procnum, errnum = getprocerr(errfn)
 
-    print "Finish %s" %(setname)
-    print "Processing = %d  Error = %d" %(procnum, errnum)
+    # print "Finish %s" %(setname)
+    # print "Processing = %d  Error = %d" %(procnum, errnum)
 
-    lightfn = xmlfn.replace(".xml", ".light.xml")
-    lightcmd = 'python src/extractXMLlight.py %s > %s' %(xmlfn, lightfn)
-    print lightcmd
-    subprocess.call(lightcmd, shell=True)
+    # lightfn = xmlfn.replace(".xml", ".light.xml")
+    # lightcmd = 'python src/extractXMLlight.py %s > %s' %(xmlfn, lightfn)
+    # print lightcmd
+    # subprocess.call(lightcmd, shell=True)
 
     # lightfnlst += [lightfn]
 
---
+++ bin/generatef_nict_main.py
@@ -0,0 +1,20 @@
+import sys
+import subprocess
+import optparse
+import os
+import generatef_nict
+
+cmdparser		= optparse.OptionParser(description="Feature generator.")
+cmdparser.add_option("--mpiopt", help = "options for mpirun")
+cmdparser.add_option("--targetset", help = "target set")
+cmdparser.add_option("--genoptions", help = "options for feature generate")
+cmdparser.add_option("--date", help = ".", default="0000")
+options, args = cmdparser.parse_args()
+
+for setname in options.targetset.split():
+    pnum = generatef_nict.getpnum(setname) -1
+    print setname, pnum
+
+    generatecmd = "mpirun -np %s /home/jun-s/src/bkp/bin/bkp %s python bin/generatef_nict.py --targetset '%s' --date %s --genoptions '%s'" %(options.mpiopt, pnum, setname, options.date, options.genoptions)
+    print generatecmd
+    subprocess.call(generatecmd, shell=True)
